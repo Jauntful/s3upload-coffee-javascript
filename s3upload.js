05adbcd294e01813bf530703041a3c475b6717f1
maximeCony <maxime.cony@gmail.com>
2015-03-13 16:03:53 -0700

    custom headers support

--- s3upload.js
+++ s3upload.js
@@ -60,6 +60,11 @@
       this_s3upload = this;
       xhr = new XMLHttpRequest();
       xhr.open('GET', this.s3_sign_put_url + '?s3_object_type=' + file.type + '&s3_object_name=' + this.s3_object_name, true);
+      if (this.headers) {
+        for (var header in this.headers) {
+          xhr.setRequestHeader(header, this.headers[header]);
+        }
+      }
       xhr.overrideMimeType('text/plain; charset=x-user-defined');
       xhr.onreadystatechange = function(e) {
         var result;
